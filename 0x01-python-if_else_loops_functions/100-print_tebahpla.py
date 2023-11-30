8555f36c4e0523f5328dd3ee097f1194feec210f
TonyReignsT <tonyreigns00@gmail.com>
2023-11-30 04:30:57 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 for char in range(ord('z'), ord('A') - 1, -1):
-    print("{}".format(chr(char)), end="")
+    print("{}".format(chr(char if char % 2 == 0 else char - ord('a') + ord('A'))), end="")
+print()
