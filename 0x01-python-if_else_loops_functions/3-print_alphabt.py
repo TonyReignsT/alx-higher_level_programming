80ff7a21e7638365c75479c1f6113368897edc02
TonyReignsT <tonyreigns00@gmail.com>
2023-11-30 00:07:31 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
-import random
 
-# Generate a random integer between -10000 and 10000
-number = random.randint(-10000, 10000)
-
-# Calculate the last digit
-if number < 0:
-    last_num = number % -10
-elif number >= 0:
-    last_num = number % 10
-
-# Check conditions based on the last digit
-if last_num > 5:
-    print(f"Last digit of {number} is {last_num} and is greater than 5")
-elif last_num == 0:
-    print(f"Last digit of {number} is {last_num} and is 0")
-else:
-    print(f"Last digit of {number} is {last_num} and is less than 6 and not 0")
+for letter in range(ord('a'), ord('z') + 1):
+    if chr(letter) not in ['q', 'e']:
+        print("{}".format(chr(letter)), end="")
+print()
